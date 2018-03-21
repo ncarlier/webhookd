43820cd9f0331e4212769876d186348bd5153c43
Nicolas Carlier <n.carlier@nunux.org>
2018-03-21 14:09:19 +0000

    fix(): fix panic due to writing into closed chan

--- pkg/worker/work_request.go
+++ pkg/worker/work_request.go
@@ -8,4 +8,18 @@ type WorkRequest struct {
 	Args        []string
 	MessageChan chan []byte
 	Timeout     int
+	Closed      bool
+}
+
+// NewWorkRequest creats new work request
+func NewWorkRequest(name, script, payload string, args []string, timeout int) *WorkRequest {
+	return &WorkRequest{
+		Name:        name,
+		Script:      script,
+		Payload:     payload,
+		Args:        args,
+		Timeout:     timeout,
+		MessageChan: make(chan []byte),
+		Closed:      false,
+	}
 }
