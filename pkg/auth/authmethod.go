d40c426e17416347e8f5401dfb65be34a7ab8e32
eternal-flame-AD <eternal_flame-AD@protonmail.com>
2018-09-03 21:32:49 +0800

    docs(security): add docstring to auth.Method interface

--- pkg/auth/authmethod.go
+++ pkg/auth/authmethod.go
@@ -4,9 +4,17 @@ import "net/http"
 
 // Method an interface describing an authentication method
 type Method interface {
+	// Called after ParseParam method.
+	// auth.Method should initialize itself here and get ready to receive requests.
+	// Logger has been initialized so it is safe to call logger methods here.
 	Init(debug bool)
+	// Return Method Usage Info
 	Usage() string
+	// Parse the parameter passed through the -authparam flag
+	// Logger is not initialized at this state so do NOT call logger methods
+	// If the parameter is unacceptable, return an error and main should exit
 	ParseParam(string) error
+	// Return a middleware to handle connections.
 	Middleware() func(http.Handler) http.Handler
 }
 
